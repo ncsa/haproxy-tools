844ad50269e3ed9987cb09b638435cccb79184a3
Andy Loftus <aloftus@gmail.com>
2026-08-07 04:16:58 +0000

    fix missing parens

--- bin/00_mk_config.sh
+++ bin/00_mk_config.sh
@@ -40,7 +40,7 @@ get_config_varnames() {
 print_live_config() {
   # Print the current state of all the CONFIG_VARS
   local _vartype _var_names
-  _var_names=( echo "${!CONFIG_NAMES[@]}" | sort )
+  _var_names=( $( echo "${!CONFIG_NAMES[@]}" | sort ) )
   for k in "${_var_names[@]}" ; do
     local -n _ref="$k"
     _vartype="${CONFIG_NAMES[$k]}"
