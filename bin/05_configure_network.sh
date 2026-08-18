4536cb9687b64f6baf1e3f3f808806f084c45968
Andy Loftus <aloftus@gmail.com>
2026-08-18 03:43:16 +0000

    fix disable puppet

--- bin/05_configure_network.sh
+++ bin/05_configure_network.sh
@@ -19,7 +19,7 @@ assert_puppet_disabled() {
   _puppet=$( which puppet )
   [[ -z "${_puppet}" ]] && return 0
   _lockfile=$( "${_puppet}" agent --configprint agent_disabled_lockfile )
-  [[ -f "${_lockfile}" ]] || "${_puppet}" agent disable "disabled by '$PRG'"
+  [[ -f "${_lockfile}" ]] || "${_puppet}" agent --disable "disabled by '$PRG'"
   [[ -f "${_lockfile}" ]] || die 'puppet is still enabled'
 }
 
